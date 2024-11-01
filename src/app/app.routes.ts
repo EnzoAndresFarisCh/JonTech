28b6225aa41e385cda39762aa8c3dd1d5eaf2ac8
EnzoAndresFarisCh <enzitofch@gmail.com>
2024-11-01 01:24:42 -0300

    rotamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [{
-    path: "", component: AppComponent
-}];
+export const routes: Routes = [
+    {path: "", component: MainComponent}, 
+    {path: "teste", component: TesteComponent}
+];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,81 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+  title = 'jonathan-tech';
+  @ViewChild("hoverDiv") divHover!: ElementRef;
+  @ViewChild("serviços") serviços!: ElementRef;
+  @ViewChild("sobreNós") sobrenós!: ElementRef;
+  @ViewChild("contato") contato!: ElementRef;
+  @ViewChild("menuMobile") menuMobile!: ElementRef
+  isMobile: boolean = window.innerWidth <= 991
+  isOpenMenuMobile: boolean = false
+  isApple: boolean = /iPad|iPhone|iPod/.test(navigator.userAgent)
+
+  ngAfterViewInit(){
+    this.observerSelector(this.serviços.nativeElement, "in-view-port")
+    this.observerSelector(this.sobrenós.nativeElement, "in-view-port")
+    this.observerSelector(this.contato.nativeElement, "in-view-port")
+    if(this.isMobile){
+      this.insertHeightInMenu()
+    }
+  }
+
+  menu: string[] = ["Serviços", "Sobre Nós", "Contato"]
+
+  hover({ target }: Event): void{
+    const {width, height } = (<HTMLElement>target).getBoundingClientRect()
+    this.divHover.nativeElement.style.left = `${(<HTMLElement>target)?.offsetLeft}px`
+    this.divHover.nativeElement.style.top = `0px`
+    this.divHover.nativeElement.style.width = `${width}px`
+    this.divHover.nativeElement.style.height = `${height}px`
+  }
+
+  observerSelector(sel: HTMLElement, classe: string) {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(classe);
+          observer.disconnect()
+        } 
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(sel);
+  }
+
+  slowMove(data: string): void{
+    this.isOpenMenuMobile = false
+    this[<"contato" | "sobrenós" | "serviços">data.split(" ").join("").toLowerCase()].nativeElement.scrollIntoView()
+  }
+
+  redirectTo(url: string){
+    window.open(url, "_blank")
+  }
+
+  openCloseMenu(){
+    this.isOpenMenuMobile = !this.isOpenMenuMobile
+  }
+
+  insertHeightInMenu(): void{
+    
+      const {offsetHeight, parentElement} = this.menuMobile?.nativeElement
+      parentElement.style.setProperty('--height-open', `${offsetHeight}px`)
+    
+  }
+}
